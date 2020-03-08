081eac3d19dd6535084d408cb44ad7e9f09fd4c6
ankumalv <ankumalv@cisco.com>
2020-03-09 00:27:25 +0530

    123

---
+++ src/test/java/MvnProject/MvnProject/HelloIndia.java
@@ -0,0 +1,8 @@
+package MvnProject.MvnProject;
+public class HelloIndia {
+	public static void main(String[]args) {
+	 System.out.println("This is for India Demo");	
+	 System.out.println("This is for second");
+	 System.out.println("This is for Third");
+	}
+}
